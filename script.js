2533d9161512ff2bc8b76197d806087a45a6af63
vaishnavirkarale <vaishnavikarale7887@gmail.com>
2025-10-30 00:42:16 +0530

    fix interval issue on start button

--- script.js
+++ script.js
@@ -3,21 +3,29 @@ const minutesElement = document.getElementById("minutes");
 
 let seconds = 0;
 let minutes = 0;
-secondsElement.textContent = String(seconds).padStart(2,"0");
+let validate = false;
+secondsElement.textContent = String(seconds).padStart(2, "0");
 
 let flag;
 function startTimer() {
-   flag = setInterval(() => {
-    seconds++;
-    secondsElement.textContent = String(seconds).padStart(2,"0");
-    if (seconds == 60) {
-      minutes++;
-      seconds = 0;
-      minutesElement.textContent = String(minutes).padStart(2,"0");
-    }
-  }, 1000);
+  if (!validate) {
+    validate=true;
+    flag = setInterval(() => {
+      seconds++;
+      secondsElement.textContent = String(seconds).padStart(2, "0");
+      if (seconds == 60) {
+        minutes++;
+        seconds = 0;
+        minutesElement.textContent = String(minutes).padStart(2, "0");
+      }
+    }, 1000);
+  }
+  else{
+    return null;
+  }
 }
 
 function stopTimer() {
-    clearInterval(flag);
+  validate = false;
+  clearInterval(flag);
 }
